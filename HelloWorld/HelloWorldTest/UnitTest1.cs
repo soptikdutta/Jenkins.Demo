129deecfcabf7d3a0cdf2f7eb787056396bc0744
US\sdutta <sdutta@deloitte.com>
2017-11-30 21:00:06 +0530

    Error Fixed

--- HelloWorld/HelloWorldTest/UnitTest1.cs
+++ HelloWorld/HelloWorldTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace HelloWorldTest
         public void TestMethod1()
         {
             //Added a comment
-            Assert.AreEqual("Hello World", Program.SendMessage());
+            Assert.AreEqual("Hello World!", Program.SendMessage());
         }
     }
 }
